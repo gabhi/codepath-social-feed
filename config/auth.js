e9fc9ad509f55d2b894cba8767b8577a685613ca
Abhijit Gaikwad <AGaikwad@walmartlabs.com>
2015-05-19 22:18:11 -0700

    initial commit

---
+++ config/auth.js
@@ -0,0 +1,19 @@
+module.exports = {
+    'development': {
+        'facebook': {
+            'consumerKey': '1439023756414410',
+            'consumerSecret': '385329b4386f28157014245242ea44ca',
+            'callbackUrl': 'http://perceptions.io:8000/auth/facebook/callback'
+        },
+        'twitter': {
+            'consumerKey': 'aSXmvtJSDlRF5j7j2HuOoW1o1',
+            'consumerSecret': 'I8bP67UExpi8ipMP1rbXuVqOoVGa35uPvqYWGhyVQnRqEReItq',
+            'callbackUrl': 'http://perceptions.io:8000/auth/twitter/callback'
+        },
+        'google': {
+            'consumerKey': '446585441765-unda5mjs6307q1pqobvhiqj87m9m2kh1.apps.googleusercontent.com',
+            'consumerSecret': '...',
+            'callbackUrl': 'http://perceptions.io:8000/auth/google/callback'
+        }
+    }
+}
